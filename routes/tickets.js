917bd91bec3b90bc6317900af3ab845ad3e550c4
aers0511 <angel.reyes215999@potros.itson.edu.mx>
2025-07-14 02:00:01 -0700

    add: tickets del mes

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middlewares/upload");
-const auth = require("../middlewares/auth");
+const auth = require("../middlewares/authMiddleware");
 
 const ticketController = require("../controllers/ticketController");
 
