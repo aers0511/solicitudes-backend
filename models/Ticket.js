1096e5c7b3229a151b52c46cb4217e6ef8581313
aers0511 <angel.reyes215999@potros.itson.edu.mx>
2025-07-14 01:00:02 -0700

    add: timestamp

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const TicketSchema = new mongoose.Schema({
-  nombreSolicitante: String,
-  correoSolicitante: String,
-  destinatario: String,
-  fechaLimite: Date,
-  location: String,
-  persistentError: Boolean,
-  issueType: String,
-  description: String,
-  image: String, // ruta archivo subido
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  status: { type: String, default: "pendiente" },
-  comments: [
-    {
-      text: String,
-      author: String,
-      date: Date,
-    },
-  ],
-});
+const TicketSchema = new mongoose.Schema(
+  {
+    nombreSolicitante: String,
+    correoSolicitante: String,
+    destinatario: String,
+    fechaLimite: Date,
+    location: String,
+    persistentError: Boolean,
+    issueType: String,
+    description: String,
+    image: String, // ruta archivo subido
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    status: { type: String, default: "pendiente" },
+    comments: [
+      {
+        text: String,
+        author: String,
+        date: Date,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Ticket", TicketSchema);
