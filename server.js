3cab0490c2918144855fde1f28b396940e7177bc
Angel Eli Reyes Santiago <angel.reyes215999@hotmail.com>
2025-09-11 15:26:52 -0700

    Cors

--- server.js
+++ server.js
@@ -15,13 +15,23 @@ const ticketRoutes = require("./routes/tickets");
 app.use("/api/auth", authRoutes);
 app.use("/api/tickets", ticketRoutes);
 
+app.use(
+  cors({
+    origin: "https://solicitudes-frontend.vercel.app", // tu frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], // permitir Authorization
+    credentials: true,
+  })
+);
+app.options("*", cors());
+
 const PORT = process.env.PORT || 4000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB conectado");
-    app.listen(PORT, () => console.log(`Servidor en puerto http://localhost:${PORT}/api`));
+    app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}/api`));
   })
   .catch((error) => {
     console.error("Error conectando a MongoDB:", error);
